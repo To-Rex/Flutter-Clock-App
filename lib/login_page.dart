c895d57ecfcf7bd45710a631057384563dfffbbb
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-24 20:54:56 +0500

    Calc Application

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,14 +7,154 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  late final _emailController = TextEditingController();
+  late final _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('Login Page'),
+          children: [
+            //email field
+            const Expanded(child: Text('')),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 221, 221, 221), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  cursorColor: Colors.deepPurpleAccent,
+                  controller: _emailController,
+                  textAlign: TextAlign.left,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 10, right: 10),
+                    border: InputBorder.none,
+                    hintText: 'Email',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            //password field
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 221, 221, 221), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  cursorColor: Colors.deepPurpleAccent,
+                  controller: _passwordController,
+                  textAlign: TextAlign.left,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 10, right: 10),
+                    border: InputBorder.none,
+                    hintText: 'Password',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            //forgot password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Colors.deepPurpleAccent,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //login button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.055,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Kirish',
+                  ),
+                ),
+              ),
+            ),
+
+            //bottom sign up text password
+            const Expanded(child: Text('')),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.deepPurpleAccent,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
           ],
         ),
       ),
