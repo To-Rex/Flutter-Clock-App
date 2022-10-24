92d09893130270a614eee648f0c59782ec39cb79
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-24 21:08:43 +0500

    Calc Application

--- lib/registeration_page.dart
+++ lib/registeration_page.dart
@@ -97,6 +97,63 @@ class _RegisterPageState extends State<RegisterPage> {
             const SizedBox(
               height: 15,
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.055,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Kirish',
+                  ),
+                ),
+              ),
+            ),
+
+            //bottom sign up text password
+            const Expanded(child: Text('')),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //start actvity registration page
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RegisterPage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(
+                        color: Colors.deepPurpleAccent,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
           ]
         )
       ),
