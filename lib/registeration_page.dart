ec6fd1c914dfcbe878b57eeab2071dbb4d668788
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-24 21:05:38 +0500

    Calc Application

--- lib/registeration_page.dart
+++ lib/registeration_page.dart
@@ -22,36 +22,83 @@ class _RegisterPageState extends State<RegisterPage> {
       body: Center(
         child: Column(
           children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                hintText: 'Email',
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 221, 221, 221), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  cursorColor: Colors.deepPurpleAccent,
+                  controller: _emailController,
+                  textAlign: TextAlign.left,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 10, right: 10),
+                    border: InputBorder.none,
+                    hintText: 'Email',
+                  ),
+                ),
               ),
             ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                hintText: 'Password',
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 221, 221, 221), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  cursorColor: Colors.deepPurpleAccent,
+                  controller: _passwordController,
+                  textAlign: TextAlign.left,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 10, right: 10),
+                    border: InputBorder.none,
+                    hintText: 'Password',
+                  ),
+                ),
               ),
             ),
-            TextField(
-              controller: _confirmPasswordController,
-              decoration: const InputDecoration(
-                hintText: 'Confirm Password',
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 221, 221),
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 221, 221, 221), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  cursorColor: Colors.deepPurpleAccent,
+                  controller: _confirmPasswordController,
+                  textAlign: TextAlign.left,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 10, right: 10),
+                    border: InputBorder.none,
+                    hintText: 'Confirm Password',
+                  ),
+                ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                if (_passwordController.text == _confirmPasswordController.text) {
-                  print('success');
-                } else {
-                  print('error');
-                }
-              },
-              child: const Text('Register'),
+            const SizedBox(
+              height: 15,
             ),
-          ],
-        ),
+          ]
+        )
       ),
     );
   }
