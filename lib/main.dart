de48529c80ae0d148c5b607f08f57a4cca09ef19
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-28 02:34:42 +0500

    Calc Application beckent

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
