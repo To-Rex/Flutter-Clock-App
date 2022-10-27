b5f44cc2710174decf023c15727ee2e73c136de8
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-27 18:03:57 +0500

    Calc Application application

--- lib/sample_page.dart
+++ lib/sample_page.dart
@@ -165,6 +165,38 @@ class _SamplePageState extends State<SamplePage> {
                     ),
                   ),
                 ),
+                //refresh button
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color.fromRGBO(33, 158, 188, 10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        times.clear();
+                        coments.clear();
+                        switchs.clear();
+                        getTemes();
+                        setState(() {
+
+                        });
+                      },
+                      child: const Text(
+                        'Refresh',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
